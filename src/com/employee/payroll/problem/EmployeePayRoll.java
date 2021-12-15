509388f1d617f0cacb0500ef3f2bfa03937cea32
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-15 16:03:15 +0530

    UC-2:- Demonstrated File-IO operations.

--- src/com/employee/payroll/problem/EmployeePayRoll.java
+++ src/com/employee/payroll/problem/EmployeePayRoll.java
@@ -7,11 +7,41 @@ public class EmployeePayRoll {
 	String name;
 	double salary;
 
+	//Default Constructor.
+	public EmployeePayRoll() {
+		
+	}
+	
 	// Parameterized Constructor
 	public EmployeePayRoll(int id, String name, double salary) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.salary = salary;
+	}
+
+	// Getter and Setter methods.
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
 		this.salary = salary;
 	}
 
--- src/com/employee/payroll/problem/EmployeePayRollService.java
+++ src/com/employee/payroll/problem/EmployeePayRollService.java
@@ -1,5 +1,9 @@
 package com.employee.payroll.problem;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -15,17 +19,29 @@ public class EmployeePayRollService {
 	}
 
 	// Main method.
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ArrayList<EmployeePayRoll> employeeArrayList = new ArrayList<>();
 
 		EmployeePayRollService employeePayRollService = new EmployeePayRollService(employeeArrayList);
 		Scanner consoleInputReader = new Scanner(System.in);
 		employeePayRollService.readEmployeeData(consoleInputReader);
 		employeePayRollService.writeEmployeeData();
+		
+		String home = "Data/Temp/temp.txt";
+		Path path = Paths.get(home);
+		
+		System.out.println(Files.exists(path));  //returns true if file exist.
+		
+//		Files.delete(path);                      //deleting file
+		
+		System.out.println(Files.exists(path));  //returns false if file does not exist.
+		
+//		Files.createDirectories(path);
+//		Files.createFile(path);
 	}
 	
 	// Reading employee data from console.
-	public void readEmployeeData(Scanner consoleInputReader) {
+	public void readEmployeeData(Scanner consoleInputReader) throws IOException {
 		System.out.println("------------WELCOME-TO-EMPLOYEE-PAYROLL------------\n");
 		System.out.print("Enter Employee ID :- ");
 		int employeeID = consoleInputReader.nextInt();
@@ -35,6 +51,18 @@ public class EmployeePayRollService {
 		double employeeSalary = consoleInputReader.nextDouble();
 		
 		employeeList.add(new EmployeePayRoll(employeeID, employeeName, employeeSalary));   // Adding data into list.
+		
+		/*** Adding employee pay-roll data into a text file. ***/
+		EmployeePayRoll employeePayRoll = new EmployeePayRoll();
+		employeePayRoll.setId(employeeID);
+		employeePayRoll.setName(employeeName);
+		employeePayRoll.setSalary(employeeSalary);
+		
+		String employeePayRollString = employeePayRoll.toString();
+		byte[] employeePayRollByteArray = employeePayRollString.getBytes();
+		Path paths = Paths.get("Data/Temp/temp.txt");
+		
+		Files.write(paths, employeePayRollByteArray);     //writing into file.
 	}
 	
 	// Writing employee data to console.
