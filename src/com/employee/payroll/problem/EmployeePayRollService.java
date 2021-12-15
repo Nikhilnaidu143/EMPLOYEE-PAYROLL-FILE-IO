51f13609b2ba3e95ab2939ca1ee99700a43a84fd
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-15 15:03:41 +0530

    START:- Displayed welcome message.

---
+++ src/com/employee/payroll/problem/EmployeePayRollService.java
@@ -0,0 +1,7 @@
+package com.employee.payroll.problem;
+
+public class EmployeePayRollService {
+	public static void main(String[] args) {
+		System.out.println("------------WELCOME-TO-EMPLOYEE-PAYROLL-----------\n");
+	}
+}
